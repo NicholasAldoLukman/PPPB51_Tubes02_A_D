0e12bd23c176394c27d3b24437f76cbf642b875a
adeliasalsabina1 <6181901029@student.unpar.ac.id>
2021-12-05 21:55:28 +0700

    commit dari caca

--- app/src/main/java/com/example/tubes2_p3b/MainActivity.java
+++ app/src/main/java/com/example/tubes2_p3b/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        this.binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
